f912a51ae59bd0bc63d4fbe0ac637261a95c0556
pacemaker-2123 <mariangelmartinez@unicesar.edu.co>
2026-05-25 18:54:21 -0500

    🎨 🚑  complete and correct National and International class definitions
    fix(entities): complete and correct National and International class definitions
    
    I added the missing charge attribute to International, fixed its constructor, and added its getter and
    setter. In National I changed the data type of the alimentation attribute and
    updated its constructor, getter and setter accordingly. I also added a toString
    method to both classes as good practice.

--- src/entities/International.java
+++ src/entities/International.java
@@ -1,12 +1,15 @@
 package entities;
 
-public class International extends Flight{
+public class International extends Flight {
     private String countryD;
     private String visa;
+    private double charge;
 
-    public International(String code, String originCity, String destinationCity, String date, String timeOut, String timeArrival, int size, int seatVacant, double price, int stateFlight, String countryD) {
+    public International(String code, String originCity, String destinationCity, String date, String timeOut, String timeArrival, int size, int seatVacant, double price, int stateFlight, String countryD, String visa, double charge) {
         super(code, originCity, destinationCity, date, timeOut, timeArrival, size, seatVacant, price, stateFlight);
         this.countryD = countryD;
+        this.visa = visa;
+        this.charge = charge;
     }
 
     public String getCountryD() {
@@ -23,5 +26,22 @@ public class International extends Flight{
 
     public void setVisa(String visa) {
         this.visa = visa;
+    }
+
+    public double getCharge() {
+        return charge;
+    }
+
+    public void setCharge(double charge) {
+        this.charge = charge;
+    }
+
+    @Override
+    public String toString() {
+        return "International{" +
+                "countryD='" + countryD + '\'' +
+                ", visa='" + visa + '\'' +
+                ", charge=" + charge +
+                '}';
     }
 }
--- src/entities/National.java
+++ src/entities/National.java
@@ -2,9 +2,9 @@ package entities;
 
 public class National extends Flight {
     private float duration;
-    private String alimentation;
+    private boolean alimentation;
 
-    public National(String code, String originCity, String destinationCity, String date, String timeOut, String timeArrival, int size, int seatVacant, double price, int stateFlight, float duration, String alimentation) {
+    public National(String code, String originCity, String destinationCity, String date, String timeOut, String timeArrival, int size, int seatVacant, double price, int stateFlight, float duration, boolean alimentation) {
         super(code, originCity, destinationCity, date, timeOut, timeArrival, size, seatVacant, price, stateFlight);
         this.duration = duration;
         this.alimentation = alimentation;
@@ -18,11 +18,19 @@ public class National extends Flight {
         this.duration = duration;
     }
 
-    public String getAlimentation() {
+    public boolean getAlimentation() {
         return alimentation;
     }
 
-    public void setAlimentation(String alimentation) {
+    public void setAlimentation(boolean alimentation) {
         this.alimentation = alimentation;
+    }
+
+    @Override
+    public String toString() {
+        return "National{" +
+                "duration=" + duration +
+                ", alimentation='" + alimentation + '\'' +
+                '}';
     }
 }
