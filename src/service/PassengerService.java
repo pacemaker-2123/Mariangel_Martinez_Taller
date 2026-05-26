0c62258558a7a5e661089d8adbabbc13f1cb9915
pacemaker-2123 <mariangelmartinez@unicesar.edu.co>
2026-05-26 09:42:30 -0500

    ✨ 🎨 Add PassengerService with validations and exceptions
    Add PassengerService with validations and exceptions

---
+++ src/service/PassengerService.java
@@ -0,0 +1,37 @@
+package service;
+import repositories.PassengerRepository;
+import entities.Passenger;
+
+public class PassengerService {
+    private PassengerRepository pR = new PassengerRepository();
+
+    public void SaveData(Passenger passenger) throws Exception{
+        //Validation of Data;
+        if ((passenger.getAge() < 0) || (passenger.getAge() >= 120)){
+            throw new Exception("LA EDAD DEBE SER MAYOR O IGUAL A 0");
+        }
+        if ((pR.findById(passenger.getId()) != null)){
+            throw new Exception("YA EXISTE UN PASAJERO CON ESTA CEDULA");
+        }
+        if ((pR.findByPassport(passenger.getPassport()) != null)){
+            throw new Exception("YA EXISTE UN PASAJERO CON ESTE PASAPORTE");
+        }
+        if (!passenger.getEmail().contains("@")){
+            throw new Exception("EL EMAIL DEBE CONTENER EL SIMBOLO | @ |");
+        }
+        pR.save(passenger);
+    }
+
+    public int totalP(){
+        return pR.totalPassenger();
+    }
+
+    public Passenger findById(long id) throws Exception{
+        Passenger p = pR.findById(id);
+        if (p == null){
+            throw new Exception("NO SE ENCONTRO AL PASAJERO");
+        }
+        return p;
+    }
+
+}
